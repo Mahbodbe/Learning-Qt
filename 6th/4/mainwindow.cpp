e1c81090cb61d51dbed23665ad11495163d0c606
Mahbodbe <mahbod2023@gmail.com>
2026-08-08 10:25:29 +0330

    6.4

---
+++ 6th/4/mainwindow.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+
+void MainWindow::mousePressEvent(QMouseEvent* event) {
+	emit mouseMoved(event->pos());
+}
+
+void MainWindow::updateMousePosition(QPoint point) {
+	int xCord = point.x();
+	int yCord = point.y();
+	QString text = QString::number(xCord) + ", " + QString::number(yCord);
+	mousePosition->setText(text);
+	statusBar()->showMessage(text);
+}
+
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
+	windowCentralWidget = new QWidget();
+	mousePosition = new QLabel("Mouse Position");
+	QVBoxLayout* innerLayout = new QVBoxLayout();
+	innerLayout->addWidget(mousePosition);
+	windowCentralWidget->setLayout(innerLayout);
+	setCentralWidget(windowCentralWidget);
+	statusBar()->showMessage("Ready");
+	connect(this, SIGNAL(mouseMoved(QPoint)), this, SLOT(updateMousePosition(QPoint)));
+}
