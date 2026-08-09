bc47d389c228c0016b0936e48248d9b1c8d6f868
Mahbodbe <mahbod2023@gmail.com>
2026-08-09 10:16:51 +0330

    proj update

--- project/1/1/QtWidgetsApplication.cpp
+++ project/1/1/QtWidgetsApplication.cpp
@@ -3,8 +3,44 @@
 QtWidgetsApplication::QtWidgetsApplication(QWidget *parent)
     : QMainWindow(parent)
 {
+
     ui.setupUi(this);
+	model = new QStandardItemModel(this);
+	//ui.appTable->setModel(model);
+	signal();
 }
 
 QtWidgetsApplication::~QtWidgetsApplication()
 {}
+
+void QtWidgetsApplication::signal() {
+	//connect(ui.actionQuit, &QAction::triggered, this, &QApplication::quit);
+	//connect(ui.actionAbout, SIGNAL(triggered()), this, SLOT(aboutDialog()));
+	//connect(ui.actionSave, SIGNAL(triggered()), this, SLOT(saveButtonClicked()));
+	//connect(ui.saveButton, SIGNAL(clicked()), this, SLOT(saveButtonClicked()));
+	connect(ui.actionAbout, &QAction::triggered, this, &QtWidgetsApplication::aboutDialog);
+	connect(ui.actionSave, &QAction::triggered, this, &QtWidgetsApplication::saveButtonClicked);
+	connect(ui.saveButton, &QPushButton::clicked, this, &QtWidgetsApplication::saveButtonClicked);
+}
+
+void QtWidgetsApplication::saveButtonClicked() {
+	int row = ui.appTable->rowCount();
+	ui.appTable->insertRow(row);
+	ui.appTable->setItem(row, 0, new QTableWidgetItem(ui.nameLineEdit->text()));
+	ui.appTable->setItem(row, 1, new QTableWidgetItem(ui.dateEdit->date().toString()));
+	ui.appTable->setItem(row, 2, new QTableWidgetItem(ui.phoneNumberLineEdit->text()));
+
+	clearfield();
+	QMessageBox::information(this, tr("RMS System"), tr("Record saved successfully!"));
+}
+
+void QtWidgetsApplication::aboutDialog()  {
+	QMessageBox::about(this, "About RMS System", "RMS System 2.0" "<p>Copyright &copy; 2005 Inc." "This is a simple application to demonstrate the use of windows, " "tool bars, menus and dialog boxes");
+}
+
+void QtWidgetsApplication::clearfield() {
+	ui.nameLineEdit->clear();
+	ui.phoneNumberLineEdit->setText("");
+	QDate dateOfBirth(1980, 1, 1);
+	ui.dateEdit->setDate(dateOfBirth);
+}
--- project/1/1/QtWidgetsApplication.h
+++ project/1/1/QtWidgetsApplication.h
@@ -1,6 +1,26 @@
 #pragma once
 
-#include <QtWidgets/QMainWindow>
+#include <QMainWindow>
+#include <QApplication>
+#include <QLabel>
+#include <QLineEdit>
+#include <QDate>
+#include <QDateEdit>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGridLayout>
+#include <QPushButton>
+#include <QMessageBox>
+#include <QAction>
+#include <QMenuBar>
+#include <QMenu>
+#include <QtGui>
+#include <Qt>
+#include <QToolBar>
+#include <QTableView>
+#include <QHeaderView>
+#include <QInputDialog>
+
 #include "ui_MainWindow.h"
 
 class QtWidgetsApplication : public QMainWindow
@@ -13,4 +33,16 @@ public:
 
 private:
     Ui::MainWindow ui;
+    void signal();
+    QAction* actionQuit;
+
+    QLineEdit* nameLineEdit;
+    QStandardItemModel* model;
+    
+
+
+private slots:
+    void saveButtonClicked();
+    void aboutDialog();
+    void clearfield();
 };
